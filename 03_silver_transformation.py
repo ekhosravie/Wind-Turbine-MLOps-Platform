8e6e9d506c77f2887bac171b3e9d8ba9ab9e2e1c
Ehsan Khosravi esfarjani <47142001+ekhosravie@users.noreply.github.com>
2026-09-11 02:45:42 +0800

    Delete 03_silver_transformation.py

--- 03_silver_transformation.py
+++
@@ -1,7 +0,0 @@
-"""
-Silver transformation orchestrator - thin wrapper around src.transformations.silver_cleaner (schema bug fixed there).
-"""
-
-from src.transformations.silver_cleaner import build_silver
-
-build_silver(spark, bronze_table=config.get_bronze_table_name(), silver_table=config.get_silver_table_name())
